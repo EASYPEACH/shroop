30e7fcd86f53ffbdb2ce1acd3fdb4309d46196d7
박진영 <122027452+jiny798@users.noreply.github.com>
2023-08-11 10:17:38 +0900

    chore: 백엔드 프로젝트 셋업 (#14)
    * chore: 백엔드 프로젝트 셋업
    
    * rename: 애플리케이션 이름 변경
    
    * rename: 테스트 파일 클래스 명 변경

---
+++ backend/src/main/java/com/easypeach/backend/ShroopApplication.java
@@ -0,0 +1,13 @@
+package com.easypeach.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShroopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShroopApplication.class, args);
+	}
+
+}
