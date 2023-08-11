8e841cb24589c2d7503dc325aa7e058613319954
박지윤 <jyun1937@naver.com>
2023-08-11 16:17:35 +0900

    design: header, footer ui 구현

--- frontend/src/layouts/default/Default.vue
+++ frontend/src/layouts/default/Default.vue
@@ -1,12 +1,13 @@
 <template>
   <v-app>
     <default-bar />
-
     <default-view />
+    <Footer />
   </v-app>
 </template>
 
 <script setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
+import DefaultBar from "./AppBar.vue";
+import DefaultView from "./View.vue";
+import Footer from "@/components/Footer.vue";
 </script>
