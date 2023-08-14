1470d3f1b667781f648cc53a9530ce207ffbd9e1
jiny798 <jyoung7988@naver.com>
2023-08-14 18:30:21 +0900

    fix: CORS를 설정한 WebConfig를 빈으로 등록

--- backend/src/main/java/com/easypeach/shroop/infra/config/WebConfig.java
+++ backend/src/main/java/com/easypeach/shroop/infra/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.easypeach.shroop.infra.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
