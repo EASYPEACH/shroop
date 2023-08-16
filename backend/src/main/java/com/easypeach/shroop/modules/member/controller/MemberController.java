2196dc7edbd18590ff0c3d018d070f42c82f5908
jyoung7988 <jyoung7988@naver.com>
2023-08-17 00:20:44 +0900

    Feat: 생성된 애노테이션 테스트

--- backend/src/main/java/com/easypeach/shroop/modules/member/controller/MemberController.java
+++ backend/src/main/java/com/easypeach/shroop/modules/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.easypeach.shroop.modules.member.controller;
 
 import com.easypeach.shroop.modules.auth.support.LoginMember;
+import com.easypeach.shroop.modules.auth.support.LoginMemberId;
 import com.easypeach.shroop.modules.member.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     @PostMapping(value = "/test")
-    public String test(@LoginMember Member member){
-        log.info("member Login ID = {} ",member.getLoginId());
-        log.info("member Nickname = {} ",member.getNickname());
+    public String test(@LoginMemberId Long memberId, @LoginMember Member member){
 
         return "ok";
     }
