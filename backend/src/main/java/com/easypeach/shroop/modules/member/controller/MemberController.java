f2f828efde3afcf5d594aaf7427410fd1b2d18b3
jyoung7988 <jyoung7988@naver.com>
2023-08-17 00:11:17 +0900

    Feat: MemberController 테스트용으로 임시 생성

--- backend/src/main/java/com/easypeach/shroop/modules/member/controller/MemberController.java
+++ backend/src/main/java/com/easypeach/shroop/modules/member/controller/MemberController.java
@@ -1,4 +1,20 @@
 package com.easypeach.shroop.modules.member.controller;
 
+import com.easypeach.shroop.modules.auth.support.LoginMember;
+import com.easypeach.shroop.modules.member.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
 public class MemberController {
+
+    @PostMapping(value = "/test")
+    public String test(@LoginMember Member member){
+        log.info("member Login ID = {} ",member.getLoginId());
+        log.info("member Nickname = {} ",member.getNickname());
+
+        return "ok";
+    }
 }
