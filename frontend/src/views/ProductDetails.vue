92dc306104ac566757e4497f74a592ba7bcc7d93
kongtaesic <xotlr333@naver.com>
2023-08-17 16:14:56 +0900

    Refactor: UI 수정
    - 전체적으로 크기를 줄였습니다.
    - 클래스명을 정리했습니다.
    - 툴팁 디자인을 수정했습니다.

--- frontend/src/views/ProductDetails.vue
+++ frontend/src/views/ProductDetails.vue
@@ -1,30 +1,20 @@
 <template>
   <section>
-    <div class="img_section-content">
+    <div class="imgSlide">
       <v-carousel>
         <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          cover
-        ></v-carousel-item>
-
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-          cover
-        ></v-carousel-item>
-
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          v-for="(productImg, idx) in productImgs"
+          :key="idx"
+          :src="productImg.imgUrl"
           cover
         ></v-carousel-item>
       </v-carousel>
-      <div class="content_div-content">
+      <div class="productContent">
         <v-card-item>
           <div>
-            <div class="item_div-title">
-              <div class="text-h3 mb-1">
-                냉장고 팔아요냉장고 팔아요냉장고 팔아요냉장고
-              </div>
-              <div class="item_div-menu">
+            <div class="productContent__item-title">
+              <div class="text-h5 pt-2">냉장고 팔아요</div>
+              <div class="productContent__side-tooltips">
                 <v-menu>
                   <template v-slot:activator="{ props }">
                     <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
@@ -35,21 +25,39 @@
                       :key="index"
                       :value="index"
                     >
-                      <v-list-item-title>{{ item.title }}</v-list-item-title>
+                      <v-list-item-title class="font-weight-bold">
+                        {{ item.title }}
+                      </v-list-item-title>
                     </v-list-item>
                   </v-list>
                 </v-menu>
               </div>
             </div>
-            <div class="text-h4 mb-1">1000원</div>
-            <div class="text-subtitle-1 mb-1">배송비 포함</div>
-            <div class="text-subtitle-1 mb-1">전자제품</div>
-            <div class="text-subtitle-1 mb-1">2023-08-09</div>
+            <div class="productContent__price">
+              <div class="text-h5 mb-1">1000원</div>
+              <div class="text-caption mb-1">배송비 포함</div>
+            </div>
+            <v-table class="mt-2">
+              <tbody>
+                <tr>
+                  <td class="text-subtitle-2">
+                    <v-icon icon="mdi-vector-point"></v-icon>카테고리
+                  </td>
+                  <td>{{ productContent.category }}</td>
+                </tr>
+                <tr>
+                  <td class="text-subtitle-2">
+                    <v-icon icon="mdi-vector-point"></v-icon>생성일자
+                  </td>
+                  <td>{{ productContent.createDate }}</td>
+                </tr>
+              </tbody>
+            </v-table>
           </div>
         </v-card-item>
-        <v-divider :thickness="4" class="border-opacity-25 mt-8"></v-divider>
-        <div class="content_div-profile-contain">
-          <div class="content_div-profile">
+        <v-divider :thickness="4" class="border-opacity-25 mb-5"></v-divider>
+        <div class="productContent__profile">
+          <div class="productContent__profile-content">
             <v-avatar>
               <v-img
                 src="https://cdn.vuetifyjs.com/images/john.jpg"
@@ -64,7 +72,7 @@
         <v-card-actions>
           <v-btn-toggle multiple>
             <v-btn @click="HandleChangeHeart()">
-              좋아요
+              <span class="font-weight-bold pb-1">103</span>
               <v-icon
                 v-if="!likeToggle"
                 icon="mdi-cards-heart-outline"
@@ -76,11 +84,12 @@
         </v-card-actions>
       </div>
     </div>
-    <div class="content_div-detail-contain">
-      <div class="content_div-alert">
+
+    <div class="productDetail">
+      <div class="productDetail__alert">
         <div>
           <v-alert color="green" variant="outlined">
-            <div class="text-h4">
+            <div class="text-h5">
               <v-icon icon="mdi-alert-decagram"></v-icon> 구매 주의 사항
             </div>
             <div>
@@ -91,8 +100,8 @@
           </v-alert>
         </div>
       </div>
-      <div class="content_div-detail">
-        <div class="text-h4 mb-8">상품 정보</div>
+      <div class="productDetail__content">
+        <div class="text-h5 mb-6 font-weight-bold">상품 정보</div>
         <v-table>
           <thead>
             <tr>
@@ -108,39 +117,21 @@
           </tbody>
         </v-table>
       </div>
-      <div class="content_div-defect">
-        <div class="text-h4 mb-10">상품 결함 정보</div>
-        <div class="text-h5 mb-1">경함 여부 : 있음</div>
-        <div class="content_div-defect-img">
+      <div class="productDetail__defect">
+        <div class="text-h5 mb-6 font-weight-bold">상품 결함 정보</div>
+        <div class="text-h6">경함 여부 : 있음</div>
+        <div class="productDetail__defect">
           <v-img
             width="300"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          ></v-img>
-          <v-img
-            width="300"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          ></v-img>
-          <v-img
-            width="300"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          ></v-img>
-          <v-img
-            width="300"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          ></v-img>
-          <v-img
-            width="300"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          ></v-img>
-          <v-img
-            width="300"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            v-for="(defectImg, idx) in defectImgs"
+            :src="defectImg.imgUrl"
+            :key="idx"
           ></v-img>
         </div>
       </div>
-      <div class="content_div-detail">
-        <div class="text-h4 mb-10">상품 기타 상세 정보</div>
-        <div class="text-h5 mb-1">
+      <div class="productDetail__content">
+        <div class="text-h5 mb-6 font-weight-bold">상품 기타 상세 정보</div>
+        <div class="text-h6">
           정보과학에서 더미 데이터는 유용한 데이터가 포함되지 않지만 공간을
           예비해두어 실제 데이터가 명목상 존재하는 것처럼 다루는 유순한 정보를
           의미한다. 더미 데이터는 테스트 및 운영 목적을 위해 플레이스홀더로
@@ -175,7 +166,28 @@ const HandleChangeHeart = () => {
   likeToggle.value = !likeToggle.value;
 };
 
+const productImgs = [
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/docks.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/hotel.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/sunshine.jpg` },
+];
+
+const defectImgs = [
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/docks.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/hotel.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/sunshine.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/sunshine.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/sunshine.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/sunshine.jpg` },
+  { imgUrl: `https://cdn.vuetifyjs.com/images/cards/sunshine.jpg` },
+];
+
 const items = [{ title: `삭제하기` }, { title: `신고하기` }];
+
+const productContent = {
+  category: `전자제품`,
+  createDate: `2023-07-14`,
+};
 
 const desserts = [
   {
@@ -195,14 +207,72 @@ const desserts = [
 
 <style lang="scss" scoped>
 section {
+  margin-top: 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-.img_section-content {
+
+.imgSlide {
   display: flex;
   justify-content: center;
   gap: 50px;
+
+  .productContent {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 400px;
+
+    .productContent__price {
+      margin-top: 15px;
+      display: flex;
+      align-items: flex-end;
+      gap: 20px;
+    }
+
+    .v-card-item {
+      .productContent__item-title {
+        display: flex;
+        justify-content: space-between;
+      }
+
+      .productContent__side-tooltips {
+        width: 48px;
+
+        .v-btn {
+          box-shadow: none;
+        }
+      }
+
+      .v-table {
+        width: 70%;
+
+        td {
+          padding: 0;
+          border: none;
+          color: gray;
+        }
+      }
+    }
+
+    .productContent__profile {
+      padding: 0.625rem 1rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .productContent__profile-content {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+      }
+    }
+
+    .v-btn {
+      width: 180px;
+    }
+  }
 }
 
 .v-carousel {
@@ -210,97 +280,60 @@ section {
   border-radius: 25px;
 }
 
-.content_div-content {
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
-  width: 600px;
-}
-
 .v-card-actions {
-  justify-content: space-around;
+  justify-content: space-between;
   .v-btn-toggle {
     height: 36px;
     border: thin solid currentColor;
   }
 }
 
-.v-btn {
-  width: 270px;
-}
-
-.content_div-profile-contain {
-  padding: 0.625rem 1rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.content_div-profile {
-  display: flex;
-  align-items: center;
-  gap: 20px;
-}
-
-.v-avatar.v-avatar--size-default {
-  --v-avatar-height: 70px;
-}
-
-.v-card-item {
-  .item_div-title {
-    display: flex;
-    justify-content: space-between;
-  }
-  .item_div-menu {
-    width: 48px;
-    // .v-list {
-    //   width: 53px;
-    // }
-  }
-}
-
-.v-table {
-  font-size: 25px;
-}
-
-.v-btn-toggle {
-  height: 36px;
-  border-color: inherit;
-}
-
-.content_div-detail-contain {
-  width: 1160px;
+.productDetail {
+  width: 940px;
   display: flex;
   flex-direction: column;
-  margin-top: 120px;
-  .content_div-alert {
+  margin-top: 100px;
+  .productDetail__alert {
+    margin-bottom: 30px;
     display: flex;
     justify-content: space-around;
     .v-alert {
-      width: 900px;
-      font-size: 25px;
+      width: 600px;
+      font-size: 20px;
       text-align: center;
     }
   }
 
-  .content_div-detail {
+  .productDetail__content {
     margin-top: 60px;
-    div {
-      // margin-top: 30px;
+
+    .v-table {
+      font-size: 20px;
     }
   }
 
-  .content_div-defect {
+  .productDetail__defect {
     margin-top: 60px;
     div {
       margin-top: 30px;
     }
-    .content_div-defect-img {
+    .productDetail__defect {
       margin-top: 0px;
       display: flex;
       gap: 20px;
       overflow: auto;
       white-space: nowrap;
+    }
+  }
+}
+
+.v-menu {
+  .v-overlay__content {
+    .v-list {
+      background: rgb(var(--v-theme-mainGray));
+      border-radius: 10px;
+      color: white;
+      padding: 0;
     }
   }
 }
