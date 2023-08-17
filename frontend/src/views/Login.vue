558f38a2560f435c7297f5d4cd4af291f650282e
JIYUN <72537762+Jiyun-Parkk@users.noreply.github.com>
2023-08-17 14:23:59 +0900

    Refactor: form 태그와  submit handler 추가 (#63)

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -7,55 +7,69 @@
       max-width="448"
       rounded="lg"
     >
-      <custom-text-input
-        label="아이디"
-        placeholder-text="아이디를 입력해주세요"
-        v-model="id"
-        :rules="[idRule.required, idRule.min, idRule.check]"
-        icon="mdi-account-outline"
-      />
-      <password-input
-        :visible="visible"
-        v-model="password"
-        :rules="[passwordRule.required, passwordRule.min, passwordRule.check]"
-        isNeedSearchState
-      />
-      <v-btn block class="mb-8 form__btn-login" size="large" variant="tonal">
-        로그인
-      </v-btn>
-      <v-card-text class="text-center">
-        슈룹회원이 아니신가요?
-        <router-link
-          to="/signup"
-          class="guide_button"
-          rel="noopener noreferrer"
+      <v-form v-model="isValid" @submit.prevent="handleSubmitLogin">
+        <custom-text-input
+          label="아이디"
+          placeholder-text="아이디를 입력해주세요"
+          v-model="id"
+          :rules="[idRule.required, idRule.min, idRule.check]"
+          icon="mdi-account-outline"
+        />
+        <password-input
+          :visible="visible"
+          v-model="password"
+          :rules="[passwordRule.required, passwordRule.min, passwordRule.check]"
+          isNeedSearchState
+        />
+        <v-btn
+          block
+          type="submit"
+          class="mb-8 form__btn-login"
+          size="large"
+          variant="tonal"
+          :disabled="!isValid"
         >
-          회원가입<v-icon icon="mdi-chevron-right"></v-icon>
-        </router-link>
-      </v-card-text>
+          로그인
+        </v-btn>
+        <v-card-text class="text-center">
+          슈룹회원이 아니신가요?
+          <router-link
+            to="/signup"
+            class="guide_button"
+            rel="noopener noreferrer"
+          >
+            회원가입<v-icon icon="mdi-chevron-right"></v-icon>
+          </router-link>
+        </v-card-text>
+      </v-form>
     </v-card>
   </div>
 </template>
 
 <script setup>
-  import { ref } from "vue";
-  import Title from "@/components/Title.vue";
-  import CustomTextInput from "@/components/Form/CustomTextInput.vue";
-  import PasswordInput from "@/components/Form/PasswordInput.vue";
-  import { idRule, passwordRule } from "@/components/Form/data/formRules";
+import { ref } from "vue";
+import Title from "@/components/Title.vue";
+import CustomTextInput from "@/components/Form/CustomTextInput.vue";
+import PasswordInput from "@/components/Form/PasswordInput.vue";
+import { idRule, passwordRule } from "@/components/Form/data/formRules";
 
-  const visible = ref(false);
-  const id = ref("");
-  const password = ref("");
+const visible = ref(false);
+const id = ref("");
+const password = ref("");
+const isValid = ref(false);
+const handleSubmitLogin = () => {
+  console.log(id.value);
+  console.log(password.value);
+};
 </script>
 
 <style lang="scss" scoped>
-  .form__btn-login {
-    background: none !important;
-    font-weight: 600;
-    color: rgb(var(--v-theme-mainGray));
-  }
-  .guide_button {
-    color: rgb(var(--v-theme-subBlue));
-  }
+.form__btn-login {
+  background: none !important;
+  font-weight: 600;
+  color: rgb(var(--v-theme-mainGray));
+}
+.guide_button {
+  color: rgb(var(--v-theme-subBlue));
+}
 </style>
