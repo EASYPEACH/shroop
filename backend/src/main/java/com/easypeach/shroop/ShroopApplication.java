58062f72cb8c21526c453a28be43841535843760
{dlwhdals5059} <dlwhdals5059@gmail.com>
2023-08-22 18:59:37 +0900

    Feat: JPA Auditing 추가

--- backend/src/main/java/com/easypeach/shroop/ShroopApplication.java
+++ backend/src/main/java/com/easypeach/shroop/ShroopApplication.java
@@ -2,8 +2,10 @@ package com.easypeach.shroop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ShroopApplication {
 
 	public static void main(String[] args) {
