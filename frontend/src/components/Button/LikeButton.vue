b32d762697d3d5a51c9aded120ee2699b8c76cc8
박지윤 <jyun1937@naver.com>
2023-08-22 11:56:07 +0900

    Feat: 좋아요 버튼 컴포넌트화

---
+++ frontend/src/components/Button/LikeButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn variant="text" @click="$emit('handle-click-like', product.id)">
+    <v-icon :icon="product.like ? 'mdi-heart' : 'mdi-heart-outline'" />
+  </v-btn>
+</template>
+
+<script setup>
+defineProps({
+  product: {
+    type: Number,
+    required: true,
+  },
+});
+defineEmits(["handle-click-like"]);
+</script>
+
+<style lang="scss" scoped>
+.v-icon {
+  color: rgb(var(--v-theme-heartRed));
+}
+</style>
