5266e16988b7185223d51507a5cff8180e8887fe
박진영 <122027452+jiny798@users.noreply.github.com>
2023-08-23 16:10:43 +0900

    Feat/ 세션 기반 인증/인가 구현 (#141)
    * Refactor: 토큰 인증 관련 불필요한 파일 삭제
    
    * Refactor: 스프링 시큐리티 인증 사용으로 로그인 메서드 삭제
    
    * Refactor: 스프링 시큐리티 인증 사용으로 로그인 로직 삭제
    
    * Refactor: 인증인가 관련 DTO를 정리한다
    
    * Feat: 세션 기반 인증을 구현한다
    
    * Test: 회원가입, 로그인 테스트 코드 작성
    
    * Fix: final 누락된 부분 추가
    
    * Refactor: 불필요한 코드 삭제
    - 테스트용으로 사용했던 URL 제거
    
    * Refactor: 회원 가입 DTO 수정
    - 회원 가입 성공 여부만 나오도록 수정

--- backend/src/main/java/com/easypeach/shroop/modules/auth/service/AuthService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/auth/service/AuthService.java
@@ -3,10 +3,7 @@ package com.easypeach.shroop.modules.auth.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.easypeach.shroop.modules.auth.dto.request.SignInRequest;
 import com.easypeach.shroop.modules.auth.dto.request.SignUpRequest;
-import com.easypeach.shroop.modules.auth.dto.response.AccessTokenResponse;
-import com.easypeach.shroop.modules.auth.dto.response.SignUpCompletedResponse;
 import com.easypeach.shroop.modules.member.domain.Member;
 import com.easypeach.shroop.modules.member.domain.MemberRepository;
 import com.easypeach.shroop.modules.member.domain.Role;
@@ -14,16 +11,15 @@ import com.easypeach.shroop.modules.member.domain.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class AuthService {
 
 	private final MemberRepository memberRepository;
-	private final JwtTokenProvider jwtTokenProvider;
 	private final PasswordEncoder passwordEncoder;
 
-	public SignUpCompletedResponse saveMember(SignUpRequest signUpRequest) {
+	public void saveMember(SignUpRequest signUpRequest) {
 		Member member = Member.createMember(signUpRequest.getLoginId()
 			, passwordEncoder.encode(signUpRequest.getPassword())
 			, signUpRequest.getNickname()
@@ -32,24 +28,6 @@ public class AuthService {
 			, 0L);
 
 		Member savedMember = memberRepository.save(member);
-		SignUpCompletedResponse signUpCompletedResponse = new SignUpCompletedResponse(member.getId(),
-			member.getNickname());
-		return signUpCompletedResponse;
-	}
 
-	public AccessTokenResponse login(SignInRequest signInRequest) {
-		String loginId = signInRequest.getLoginId();
-		String password = signInRequest.getPassword();
-		Member member = memberRepository.findByLoginId(loginId)
-			.orElseThrow(() -> new IllegalArgumentException("아이디 및 비밀번호가 맞지 않습니다."));
-
-		if (!passwordEncoder.matches(password, member.getPassword())) {
-			throw new IllegalArgumentException("아이디 및 비밀번호가 맞지 않습니다.");
-		}
-
-		AccessTokenResponse accessTokenResponse
-			= new AccessTokenResponse(
-			jwtTokenProvider.createToken(member.getLoginId(), member.getNickname(), member.getRole()));
-		return accessTokenResponse;
 	}
 }
