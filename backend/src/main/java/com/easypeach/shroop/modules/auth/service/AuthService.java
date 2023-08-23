3e35bd49b48d16fa400eb5672885baf5fe2e2b86
jiny798 <jyoung7988@naver.com>
2023-08-23 12:22:10 +0900

    Refactor: 스프링 시큐리티 인증 사용으로 로그인 로직 삭제

--- backend/src/main/java/com/easypeach/shroop/modules/auth/service/AuthService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/auth/service/AuthService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.easypeach.shroop.modules.auth.dto.request.SignInRequest;
 import com.easypeach.shroop.modules.auth.dto.request.SignUpRequest;
-import com.easypeach.shroop.modules.auth.dto.response.AccessTokenResponse;
+import com.easypeach.shroop.modules.auth.dto.response.LoginSuccessResponse;
 import com.easypeach.shroop.modules.auth.dto.response.SignUpCompletedResponse;
 import com.easypeach.shroop.modules.member.domain.Member;
 import com.easypeach.shroop.modules.member.domain.MemberRepository;
@@ -14,13 +14,12 @@ import com.easypeach.shroop.modules.member.domain.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class AuthService {
 
 	private final MemberRepository memberRepository;
-	private final JwtTokenProvider jwtTokenProvider;
 	private final PasswordEncoder passwordEncoder;
 
 	public SignUpCompletedResponse saveMember(SignUpRequest signUpRequest) {
@@ -35,21 +34,5 @@ public class AuthService {
 		SignUpCompletedResponse signUpCompletedResponse = new SignUpCompletedResponse(member.getId(),
 			member.getNickname());
 		return signUpCompletedResponse;
-	}
-
-	public AccessTokenResponse login(SignInRequest signInRequest) {
-		String loginId = signInRequest.getLoginId();
-		String password = signInRequest.getPassword();
-		Member member = memberRepository.findByLoginId(loginId)
-			.orElseThrow(() -> new IllegalArgumentException("아이디 및 비밀번호가 맞지 않습니다."));
-
-		if (!passwordEncoder.matches(password, member.getPassword())) {
-			throw new IllegalArgumentException("아이디 및 비밀번호가 맞지 않습니다.");
-		}
-
-		AccessTokenResponse accessTokenResponse
-			= new AccessTokenResponse(
-			jwtTokenProvider.createToken(member.getLoginId(), member.getNickname(), member.getRole()));
-		return accessTokenResponse;
 	}
 }
