3b76c639f1b801d03ddbe7a19f9927ce36976158
박지윤 <jyun1937@naver.com>
2023-08-24 18:13:25 +0900

    Feat: 상품 등급 및 상태 enum

---
+++ backend/src/main/java/com/easypeach/shroop/modules/product/domain/ProductGrade.java
@@ -0,0 +1,13 @@
+package com.easypeach.shroop.modules.product.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum ProductGrade {
+	UPPER("상"), MIDDLE("중"), LOWER("하");
+	private final String grade;
+
+	ProductGrade(String grade) {
+		this.grade = grade;
+	}
+}
--- backend/src/main/java/com/easypeach/shroop/modules/product/domain/ProductStatus.java
+++ backend/src/main/java/com/easypeach/shroop/modules/product/domain/ProductStatus.java
@@ -1,4 +1,21 @@
 package com.easypeach.shroop.modules.product.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum ProductStatus {
+
+	SELLING("판매중"),
+	PURCHASE_REQUEST("거래요청"),
+	SHIPPING("베송중"),
+	DELIVERY_COMPLETE("배송완료"),
+	TRANSACTION_COMPLETE("거래완료"),
+	RETURN_REQUEST("반품요청"),
+	RETURN_COMPLETE("반품완료");
+	private final String status;
+
+	ProductStatus(String status) {
+		this.status = status;
+	}
+
 }
