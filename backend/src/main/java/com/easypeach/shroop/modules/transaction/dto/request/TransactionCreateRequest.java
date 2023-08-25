a3618640abfc71965c8e4c490a1b9dea7ce8de1d
JongMin <dlwhdals5059@gmail.com>
2023-08-25 12:18:56 +0900

    Refactor: 상품 결제 API 리팩토링 (#167)
    * Refactor: Get 기능 추가 및 Validate 추가, Final 추가
    
    * Refactor: ResponseEntity로 status 추가
    
    * Refactor: 컨트롤러 파라미터에 final 추가

--- backend/src/main/java/com/easypeach/shroop/modules/transaction/dto/request/TransactionCreateRequest.java
+++ backend/src/main/java/com/easypeach/shroop/modules/transaction/dto/request/TransactionCreateRequest.java
@@ -1,5 +1,9 @@
 package com.easypeach.shroop.modules.transaction.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionCreateRequest {
+
+	@NotBlank
+	@Size(min = 2, max = 255, message = "최소 2자 이상 입력해주세요")
 	private String buyerName;
+
+	@NotBlank
+	@Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
 	private String buyerPhoneNumber;
+
+	@NotBlank
+	@Size(min = 5, max = 255, message = "최소 5자 이상 입력해주세요")
 	private String buyerLocation;
 }
