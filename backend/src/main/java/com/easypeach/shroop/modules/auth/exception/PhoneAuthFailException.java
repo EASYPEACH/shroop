0158dd6f208d81c1ff142652d8c2f366ace0ce2b
jiny798 <jyoung7988@naver.com>
2023-08-26 15:49:33 +0900

    Feat: 폰 인증 실패 exception 추가

---
+++ backend/src/main/java/com/easypeach/shroop/modules/auth/exception/PhoneAuthFailException.java
@@ -0,0 +1,7 @@
+package com.easypeach.shroop.modules.auth.exception;
+
+public class PhoneAuthFailException extends RuntimeException {
+	public PhoneAuthFailException(String message) {
+		super(message);
+	}
+}
