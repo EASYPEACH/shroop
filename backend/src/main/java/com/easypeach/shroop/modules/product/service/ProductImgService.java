a17912b4b202b9fc9146735e6fcb429cdfcc6317
박지윤 <jyun1937@naver.com>
2023-08-26 12:21:20 +0900

    Feat: 카테고리 수정 api 구현

--- backend/src/main/java/com/easypeach/shroop/modules/product/service/ProductImgService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/product/service/ProductImgService.java
@@ -1,39 +1,76 @@
 package com.easypeach.shroop.modules.product.service;
 
-import com.easypeach.shroop.infra.s3.service.S3UploadService;
-import com.easypeach.shroop.modules.product.domain.Product;
-import com.easypeach.shroop.modules.product.domain.ProductImg;
-import com.easypeach.shroop.modules.product.respository.ProductImgRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import com.easypeach.shroop.infra.s3.service.S3UploadService;
+import com.easypeach.shroop.modules.product.domain.Product;
+import com.easypeach.shroop.modules.product.domain.ProductImg;
+import com.easypeach.shroop.modules.product.respository.ProductImgRepository;
+import com.easypeach.shroop.modules.product.respository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class ProductImgService {
-    private final ProductImgRepository productImgRepository;
-    private final S3UploadService s3UploadService;
-    
-    @Transactional
-    public void saveProductImg(final Product product, final List<MultipartFile> productImgList) {
-        List<ProductImg> imgList = new ArrayList<>();
-        try {
-            for (MultipartFile multipartFile : productImgList) {
-                String uploadUrl = s3UploadService.saveFile(multipartFile);
-                imgList.add(ProductImg.createProductImg(product, uploadUrl, product.isDefect()));
-            }
-            productImgRepository.saveAll(imgList);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private final ProductImgRepository productImgRepository;
+	private final ProductRepository productRepository;
+	private final S3UploadService s3UploadService;
+
+	@Transactional
+	public void saveProductImg(List<MultipartFile> productImgList,
+		List<MultipartFile> defectImgList, Product product, boolean isDefect) {
+		try {
+			List<ProductImg> imgList = insertImgList(productImgList, defectImgList, product, isDefect);
+			productImgRepository.saveAll(imgList);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Transactional
+	public void updateProductImgList(List<MultipartFile> productImgList,
+		List<MultipartFile> defectImgList, Long productId, boolean isDefect) {
+		Product product = productRepository.findById(productId).get();
+		try {
+			List<ProductImg> imgList = insertImgList(productImgList, defectImgList, product, isDefect);
+			product.getProductImgList().clear();
+			for (ProductImg img : imgList) {
+				img.setProduct(product);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<ProductImg> insertImgList(List<MultipartFile> productImgList, List<MultipartFile> defectImgList,
+		Product product, boolean isDefect) throws IOException {
+		List<ProductImg> imgList = new ArrayList<>();
+		imgList.addAll(createImgList(productImgList, product, false));
+		if (isDefect) {
+			imgList.addAll(createImgList(defectImgList, product, true));
+		}
+		return imgList;
+	}
+
+	public List<ProductImg> createImgList(List<MultipartFile> requestImgList,
+		Product product, boolean isDefect) throws
+		IOException {
+		List<ProductImg> imgList = new ArrayList<>();
+		for (MultipartFile multipartFile : requestImgList) {
+			String uploadUrl = s3UploadService.saveFile(multipartFile);
+			imgList.add(ProductImg.createProductImg(product, uploadUrl, isDefect));
+		}
+		return imgList;
+	}
 
 }
--- backend/src/main/java/com/easypeach/shroop/modules/product/service/ProductService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/product/service/ProductService.java
@@ -1,7 +1,5 @@
 package com.easypeach.shroop.modules.product.service;
 
-import java.time.LocalDate;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,9 +7,7 @@ import com.easypeach.shroop.modules.member.domain.Member;
 import com.easypeach.shroop.modules.member.domain.MemberRepository;
 import com.easypeach.shroop.modules.product.domain.Category;
 import com.easypeach.shroop.modules.product.domain.Product;
-import com.easypeach.shroop.modules.product.domain.ProductGrade;
 import com.easypeach.shroop.modules.product.domain.ProductImg;
-import com.easypeach.shroop.modules.product.domain.ProductStatus;
 import com.easypeach.shroop.modules.product.dto.request.ProductRequest;
 import com.easypeach.shroop.modules.product.respository.CategoryRepository;
 import com.easypeach.shroop.modules.product.respository.ProductRepository;
@@ -27,25 +23,13 @@ public class ProductService {
 	private final ProductRepository productRepository;
 	private final MemberRepository memberRepository;
 	private final CategoryRepository categoryRepository;
+	private final ProductImgService productImgService;
 
 	@Transactional
 	public Product saveProduct(Long memberId, ProductRequest productRequest) {
-
-		Member member = memberRepository.findById(memberId).get();
+		Member seller = memberRepository.findById(memberId).get();
 		Category category = categoryRepository.findById(productRequest.getCategoryId()).get();
-
-		String title = productRequest.getTitle();
-		ProductStatus productStatus = productRequest.getProductStatus();
-		String brand = productRequest.getBrand();
-		Long price = productRequest.getPrice();
-		boolean isCheckedDeliveryFee = productRequest.isCheckedDeliveryFee();
-		LocalDate purchaseDate = productRequest.getPurchaseDate();
-		ProductGrade productGrade = productRequest.getProductGrade();
-		boolean isDefect = productRequest.isDefect();
-		String saleReason = productRequest.getSaleReason();
-		String content = productRequest.getContent();
-		Product product = Product.createProduct(member, title, productStatus, category, brand, price,
-			isCheckedDeliveryFee, purchaseDate, productGrade, isDefect, saleReason, content);
+		Product product = Product.createProduct(seller, productRequest, category);
 		return productRepository.save(product);
 	}
 
@@ -60,6 +44,14 @@ public class ProductService {
 		Product findProduct = productRepository.findById(product.getId()).orElseThrow(() -> new RuntimeException(""));
 		findProduct.getProductImgList().get(0).getId();
 		return findProduct.getProductImgList().get(0);
+	}
+
+	@Transactional
+	public Product updateProduct(Long productId, ProductRequest productRequest) {
+		Product product = productRepository.findById(productId).get();
+		Category category = categoryRepository.findById(productRequest.getCategoryId()).get();
+		product.updateProduct(productRequest, category);
+		return product;
 	}
 
 }
