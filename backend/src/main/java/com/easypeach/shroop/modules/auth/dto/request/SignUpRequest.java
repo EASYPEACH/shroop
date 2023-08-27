be2ad50745983be0d3ac7e8545a293660ab7dd0d
jyoung7988 <jyoung7988@naver.com>
2023-08-27 23:03:44 +0900

    Feat: 휴대전화 본인 인증 후 가입하는 로직 작성

--- backend/src/main/java/com/easypeach/shroop/modules/auth/dto/request/SignUpRequest.java
+++ backend/src/main/java/com/easypeach/shroop/modules/auth/dto/request/SignUpRequest.java
@@ -15,45 +15,53 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpRequest {
 
-	@NotBlank
-	@Size(min = 5, max = 255, message = "최소 5자 이상 입력해주세요")
-	@Pattern(regexp = "^[a-zA-Z0-9]*$")
-	private String loginId;
+    @NotBlank
+    @Size(min = 5, max = 255, message = "최소 5자 이상 입력해주세요")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
+    private String loginId;
 
-	@NotBlank
-	@Size(min = 5, max = 255, message = "최소 5자 이상 입력해주세요")
-	@Pattern(regexp = "^.*(?=^.{8,30}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$")
-	private String password;
+    @NotBlank
+    @Size(min = 5, max = 255, message = "최소 5자 이상 입력해주세요")
+    @Pattern(regexp = "^.*(?=^.{8,30}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$")
+    private String password;
 
-	@NotBlank
-	@Size(min = 2, max = 255, message = "최소 2자 이상 입력해주세요")
-	@Pattern(regexp = "^[a-zA-Z0-9가-힣]*$")
-	private String nickname;
+    @NotBlank
+    @Size(min = 2, max = 255, message = "최소 2자 이상 입력해주세요")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$")
+    private String nickname;
 
-	@NotBlank
-	@Pattern(regexp = "^010\\d{7,8}$")
-	private String phoneNumber;
+    @NotBlank
+    @Pattern(regexp = "^010\\d{7,8}$")
+    private String phoneNumber;
 
-	@NotNull
-	@AssertTrue(message = "약관에 동의가 필요합니다")
-	private Boolean agreeShroop;
+    @NotNull
+    @AssertTrue(message = "약관에 동의가 필요합니다")
+    private Boolean agreeShroop;
 
-	@NotNull
-	@AssertTrue(message = "약관에 동의가 필요합니다")
-	private Boolean agreePersonal;
+    @NotNull
+    @AssertTrue(message = "약관에 동의가 필요합니다")
+    private Boolean agreePersonal;
 
-	@NotNull
-	@AssertTrue(message = "약관에 동의가 필요합니다")
-	private Boolean agreeIdentify;
+    @NotNull
+    @AssertTrue(message = "약관에 동의가 필요합니다")
+    private Boolean agreeIdentify;
 
-	public SignUpRequest(String loginId, String password, String nickname, String phoneNumber
-		, Boolean agreeShroop, Boolean agreePersonal, Boolean agreeIdentify) {
-		this.loginId = loginId;
-		this.password = password;
-		this.nickname = nickname;
-		this.phoneNumber = phoneNumber;
-		this.agreeShroop = agreeShroop;
-		this.agreePersonal = agreePersonal;
-		this.agreeIdentify = agreeIdentify;
-	}
+    @NotNull
+    private long uuid;
+
+    @NotBlank
+    private String phoneAuthNumber;
+
+    public SignUpRequest(String loginId, String password, String nickname, String phoneNumber
+            , Boolean agreeShroop, Boolean agreePersonal, Boolean agreeIdentify, long uuid, String phoneAuthNumber) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.agreeShroop = agreeShroop;
+        this.agreePersonal = agreePersonal;
+        this.agreeIdentify = agreeIdentify;
+        this.uuid = uuid;
+        this.phoneAuthNumber = phoneAuthNumber;
+    }
 }
