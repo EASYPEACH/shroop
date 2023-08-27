b01193f837d91081908a77c90f99684d230e1574
TAESIC KONG <81614820+xotlr333@users.noreply.github.com>
2023-08-27 12:51:55 +0900

    Refactor: 신고하기 API 테스트 코드를 리팩토링한다 (#180)
    * Refactor: 테스트 코드 리팩토링
    
    * Refactor: JpaAuding 관련 어노테이션 수정
    
    JpaAuding 관련 어노테이션인 @MockBean(JpaMetamodelMappingContext.class)는 테스트 과정에서 공통으로 사용할 예정이여서 ControllerTest 클래스에 작성했다.

--- backend/src/test/java/com/easypeach/shroop/modules/common/ControllerTest.java
+++ backend/src/test/java/com/easypeach/shroop/modules/common/ControllerTest.java
@@ -1,22 +1,19 @@
 package com.easypeach.shroop.modules.common;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.easypeach.shroop.infra.security.filter.JsonUsernamePasswordAuthenticationFilter;
 import com.easypeach.shroop.modules.auth.controller.AuthController;
 import com.easypeach.shroop.modules.auth.service.AuthService;
-import com.easypeach.shroop.modules.member.controller.MemberController;
 import com.easypeach.shroop.modules.member.domain.MemberRepository;
-import com.easypeach.shroop.modules.report.controller.ReportController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureRestDocs
 @WebMvcTest({
 	AuthController.class
