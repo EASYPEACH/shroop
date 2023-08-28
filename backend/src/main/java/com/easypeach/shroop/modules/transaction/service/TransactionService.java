8b61407cf0b60648ee38478e1e864ffa41bf17c3
{dlwhdals5059} <dlwhdals5059@gmail.com>
2023-08-28 17:25:15 +0900

    Feat: 상품 결제 API 테스트 코드 작성 및 컨트롤러에서 서비스 단으로 메소드 위임

--- backend/src/main/java/com/easypeach/shroop/modules/transaction/service/TransactionService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/transaction/service/TransactionService.java
@@ -6,19 +6,24 @@ import org.springframework.transaction.annotation.Transactional;
 import com.easypeach.shroop.modules.member.domain.Member;
 import com.easypeach.shroop.modules.member.service.MemberService;
 import com.easypeach.shroop.modules.product.domain.Product;
+import com.easypeach.shroop.modules.product.domain.ProductImg;
 import com.easypeach.shroop.modules.product.service.ProductService;
 import com.easypeach.shroop.modules.transaction.domain.Transaction;
 import com.easypeach.shroop.modules.transaction.domain.TransactionRepository;
 import com.easypeach.shroop.modules.transaction.domain.TransactionStatus;
 import com.easypeach.shroop.modules.transaction.dto.request.TransactionCreateRequest;
 import com.easypeach.shroop.modules.transaction.dto.response.BuyerResponse;
+import com.easypeach.shroop.modules.transaction.dto.response.TransactionCreatedResponse;
+import com.easypeach.shroop.modules.transaction.dto.response.TransactionInfoResponse;
 import com.easypeach.shroop.modules.transaction.exception.SellerPurchaseException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class TransactionService {
 	private final TransactionRepository transactionRepository;
 	private final MemberService memberService;
@@ -54,6 +59,40 @@ public class TransactionService {
 		}
 	}
 
+	public TransactionInfoResponse createTransactionInfoResponse(Long productId, Member member) {
+		Product product = productService.findByProductId(productId);
+		ProductImg productImg = productService.getProductImg(product);
+		String productImgUrl = productImg.getProductImgUrl();
+		Member findedMember = memberService.findById(member.getId());
+
+		return new TransactionInfoResponse(productImgUrl, product.getTitle(),
+			product.getPrice(), findedMember.getPoint());
+	}
+
+	@Transactional
+	public void createTransaction(TransactionCreateRequest transactionCreateRequest, Long productId, Member member) {
+		Product product = productService.findByProductId(productId);
+		checkSeller(member, product.getSeller());
+
+		saveTransaction(productId, member.getId(), transactionCreateRequest);
+		subtractPoint(productId, member.getId());
+
+		// phoneAuthService.sendSms(product.getSeller().getPhoneNumber(),
+		// 	String.format("%s 판매자님 %s 상품이 결제되었습니다.", product.getSeller().getNickname(), product.getTitle()));
+
+		log.info("{} 판매자님 {} 상품이 결제되었습니다.", product.getSeller().getNickname(), product.getTitle());
+	}
+
+	public TransactionCreatedResponse createTransactionCreatedResponse(Long productId) {
+		Product product = productService.findByProductId(productId);
+		Transaction transaction = product.getTransaction();
+		ProductImg productImg = productService.getProductImg(product);
+		String productImgUrl = productImg.getProductImgUrl();
+
+		return new TransactionCreatedResponse(transaction.getId(), productImgUrl, product.getTitle(),
+			product.getPrice(),
+			transaction.getBuyerName(), transaction.getBuyerLocation(), transaction.getBuyerPhoneNumber());
+	}
 	public BuyerResponse getBuyer(final Long productId) {
 		Transaction transaction = findByProductId(productId);
 		return new BuyerResponse(
--- backend/src/test/java/com/easypeach/shroop/modules/transaction/controller/TransactionControllerTest.java
+++ backend/src/test/java/com/easypeach/shroop/modules/transaction/controller/TransactionControllerTest.java
@@ -12,11 +12,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.easypeach.shroop.modules.common.ControllerTest;
 import com.easypeach.shroop.modules.product.service.ProductService;
+import com.easypeach.shroop.modules.transaction.dto.request.TransactionCreateRequest;
 import com.easypeach.shroop.modules.transaction.dto.response.BuyerResponse;
+import com.easypeach.shroop.modules.transaction.dto.response.TransactionCreatedResponse;
+import com.easypeach.shroop.modules.transaction.dto.response.TransactionInfoResponse;
 import com.easypeach.shroop.modules.transaction.service.TransactionService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(TransactionController.class)
 class TransactionControllerTest extends ControllerTest {
@@ -26,6 +31,67 @@ class TransactionControllerTest extends ControllerTest {
 
 	@MockBean
 	ProductService productService;
+
+	@Test
+	@DisplayName("결제창 테스트 코드")
+	void getBuyingForm() throws Exception {
+
+		TransactionInfoResponse dto = new TransactionInfoResponse("aaaa", "상품", 10000L, 20000L);
+
+		given(transactionService.createTransactionInfoResponse(any(), any())).willReturn(dto);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/buying/1")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.productImgUrl").value("aaaa"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.title").value("상품"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.price").value(10000L))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.point").value(20000L));
+
+	}
+
+	@Test
+	@DisplayName("결제하기 테스트 코드")
+	void buyingProduct() throws Exception {
+		doNothing().when(transactionService).createTransaction(any(), any(), any());
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		TransactionCreateRequest transactionCreateRequest = new TransactionCreateRequest("이종만", "01050505050",
+			"강서구에살아");
+		String json = objectMapper.writeValueAsString(transactionCreateRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/buying/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+			.andExpect(status().isOk())
+			.andDo(document("buying",
+				responseFields(
+					fieldWithPath("message").description("결제가 완료되었습니다.")
+				)))
+			.andDo(print());
+	}
+
+	@Test
+	@DisplayName("결제 완료창 테스트 코드")
+	void getBuyingCompletedForm() throws Exception {
+		TransactionCreatedResponse transactionCreatedResponse = new TransactionCreatedResponse(1L, "url", "title",
+			30000L, "이종문", "01050502222", "강서구에삽니다");
+
+		given(transactionService.createTransactionCreatedResponse(any())).willReturn(transactionCreatedResponse);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/buying/completed/1")
+				.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.transactionId").value(1L))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.productImgUrl").value("url"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.productTitle").value("title"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.productPrice").value(30000L))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.buyerName").value("이종문"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.buyerPhoneNumber").value("01050502222"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.buyerLocation").value("강서구에삽니다"))
+			.andDo(print());
+
+	}
 
 	@DisplayName("구매자정보 조회")
 	@Test
