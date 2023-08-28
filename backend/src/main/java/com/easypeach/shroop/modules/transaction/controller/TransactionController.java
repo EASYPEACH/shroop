4d0d6eb225ce3cc55dfc5584c50d9ebdd76df3b8
JongMin <dlwhdals5059@gmail.com>
2023-08-28 12:18:31 +0900

    Refactor: 예외 처리 및 컨트롤러 리팩토링 (#194)
    * Refactor: 예외 처리 및 컨트롤러 리팩토링
    
    * Bug: phoneAuthService 주석 처리
    
    * Refactor: 포인트 업데이트 메서드 이름 변경
    
    * Refactor: 판매자 확인 메서드 이름 변경

--- backend/src/main/java/com/easypeach/shroop/modules/transaction/controller/TransactionController.java
+++ backend/src/main/java/com/easypeach/shroop/modules/transaction/controller/TransactionController.java
@@ -2,6 +2,7 @@ package com.easypeach.shroop.modules.transaction.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,10 +34,11 @@ public class TransactionController {
 	private final ProductService productService;
 	private final MemberService memberService;
 	private final TransactionService transactionService;
+	// private final PhoneAuthService phoneAuthService;
 
 	@GetMapping("/{productId}")
-	public ResponseEntity<TransactionInfoResponse> getBuyingForm(final @PathVariable Long productId,
-		final @LoginMember Member member) {
+	public ResponseEntity<TransactionInfoResponse> getBuyingForm(@PathVariable Long productId,
+		@LoginMember Member member) {
 		Product product = productService.findByProductId(productId);
 		ProductImg productImg = productService.getProductImg(product);
 		String productImgUrl = productImg.getProductImgUrl();
@@ -48,14 +50,19 @@ public class TransactionController {
 
 	@PostMapping("/{productId}")
 	public ResponseEntity<BasicResponse> buyingProduct(
-		final @RequestBody TransactionCreateRequest transactionCreateRequest,
-		final @PathVariable Long productId, final @LoginMember Member member) {
+		@Validated @RequestBody TransactionCreateRequest transactionCreateRequest,
+		@PathVariable Long productId, final @LoginMember Member member) {
 
 		Product product = productService.findByProductId(productId);
-		Member buyer = memberService.findById(member.getId());
+		transactionService.checkSeller(member, product.getSeller());
 
-		transactionService.saveTransaction(product, buyer, transactionCreateRequest);
-		transactionService.subtractPoint(product, buyer);
+		transactionService.saveTransaction(productId, member.getId(), transactionCreateRequest);
+		transactionService.subtractPoint(productId, member.getId());
+
+		// phoneAuthService.sendSms(product.getSeller().getPhoneNumber(),
+		// 	String.format("%s 판매자님 %s 상품이 결제되었습니다.", product.getSeller().getNickname(), product.getTitle()));
+
+		log.info("{} 판매자님 {} 상품이 결제되었습니다.", product.getSeller().getNickname(), product.getTitle());
 
 		return ResponseEntity.status(HttpStatus.OK).body(new BasicResponse("결제가 완료되었습니다."));
 
