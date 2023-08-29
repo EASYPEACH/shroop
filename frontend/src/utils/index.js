5165b30fc8cb4c3e1220c9ad5077f735b67e7d77
박지윤 <jyun1937@naver.com>
2023-08-29 20:40:53 +0900

    Feat: date format 유틸 함수 추가

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,4 +1,7 @@
 export { changeFiles } from "./changeFiles";
 export { changeImageToData } from "./changeImageToData";
 export { deleteImage } from "./deleteImage";
-export { multipartFormData } from "./multipartFormData";
+export { multipartFormDataFile } from "./multipartFormDataFile";
+export { multipartFormDataJson } from "./multipartFormDataJson";
+export { formatDate } from "./formatDate";
+export { changeUrlToFiles } from "./changeUrlToFiles";
