9b9dc361cccf719d24756f7e2dfbb8220187b716
kongtaesic <xotlr333@naver.com>
2023-08-29 13:11:00 +0900

    Feat: 반품 요청 시 거래 상태 변경

--- backend/src/main/java/com/easypeach/shroop/modules/product/service/ProductReturnService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/product/service/ProductReturnService.java
@@ -10,6 +10,9 @@ import com.easypeach.shroop.modules.product.domain.Product;
 import com.easypeach.shroop.modules.product.domain.ProductReturn;
 import com.easypeach.shroop.modules.product.dto.request.ProductReturnRequest;
 import com.easypeach.shroop.modules.product.respository.ProductReturnRepository;
+import com.easypeach.shroop.modules.transaction.domain.Transaction;
+import com.easypeach.shroop.modules.transaction.domain.TransactionStatus;
+import com.easypeach.shroop.modules.transaction.service.TransactionService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +27,8 @@ public class ProductReturnService {
 
 	private final ProductReturnImgService productReturnImgService;
 
+	private final TransactionService transactionService;
+
 	@Transactional
 	public void saveProductReturn(final Long memberId, final Long productId,
 		final ProductReturnRequest productReturnRequest,
@@ -32,6 +37,9 @@ public class ProductReturnService {
 		ProductReturn productReturn = ProductReturn.createProductReturn(product, productReturnRequest.getContent());
 		ProductReturn newProductReturn = productReturnRepository.save(productReturn);
 
+		Transaction transaction = transactionService.findByProductId(productId);
+		transaction.updateStatus(TransactionStatus.RETURN_REQUEST);
+		
 		productReturnImgService.saveProductReturnImg(newProductReturn, productReturnImgList);
 
 	}
