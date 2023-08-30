5e5724d5d3f6f38b1d85748515e244a79fb6e613
jiny798 <jyoung7988@naver.com>
2023-08-30 14:40:52 +0900

    Refactor: 공통 컨트롤러 예외처리 정리

--- backend/src/main/java/com/easypeach/shroop/modules/global/exception/ExceptionControllerAdvice.java
+++ backend/src/main/java/com/easypeach/shroop/modules/global/exception/ExceptionControllerAdvice.java
@@ -12,9 +12,13 @@ import com.easypeach.shroop.modules.global.exception.dto.ErrorResponse;
 import com.easypeach.shroop.modules.member.exception.DuplicateValueException;
 import com.easypeach.shroop.modules.member.exception.MemberNotExistException;
 import com.easypeach.shroop.modules.member.exception.PasswordNotMatchException;
+import com.easypeach.shroop.modules.product.exception.CategoryNotFoundException;
 import com.easypeach.shroop.modules.product.exception.ProductImgLengthException;
 import com.easypeach.shroop.modules.transaction.exception.SellerPurchaseException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
@@ -28,35 +32,36 @@ public class ExceptionControllerAdvice {
 		return ResponseEntity.badRequest().body(errorResponse);
 	}
 
-	@ExceptionHandler(SellerPurchaseException.class)
-	public ResponseEntity<ErrorResponse> handleInternalException(final RuntimeException e) {
-		String errorMessage = e.getMessage();
-		ErrorResponse errorResponse = new ErrorResponse(errorMessage);
-
-		return ResponseEntity.internalServerError().body(errorResponse);
-	}
-
 	@ExceptionHandler({
 		MemberNotExistException.class,
 		PhoneAuthNotExistException.class,
-		ProductImgLengthException.class,
-		DuplicateValueException.class,
-		PasswordNotMatchException.class
+		CategoryNotFoundException.class
 	})
-	public ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException e) {
+	public ResponseEntity<ErrorResponse> handleNotExistException(final RuntimeException e) {
 		String errorMessage = e.getMessage();
 		ErrorResponse errorResponse = new ErrorResponse(errorMessage);
-
-		return ResponseEntity.badRequest().body(errorResponse);
+		return ResponseEntity.status(404).body(errorResponse);
 	}
 
 	@ExceptionHandler({
+		SellerPurchaseException.class,
+		ProductImgLengthException.class,
+		DuplicateValueException.class,
+		PasswordNotMatchException.class,
 		PhoneAuthFailException.class
 	})
 	public ResponseEntity<ErrorResponse> handleAuthException(final RuntimeException e) {
 		String errorMessage = e.getMessage();
 		ErrorResponse errorResponse = new ErrorResponse(errorMessage);
-
 		return ResponseEntity.badRequest().body(errorResponse);
 	}
+
+	@ExceptionHandler
+	public ResponseEntity<ErrorResponse> handleException(final Exception e) {
+		String errorMessage = e.getMessage();
+		log.error(e.getMessage());
+		ErrorResponse errorResponse = new ErrorResponse("내부 서버에 문제가 발생하여 확인 중 입니다");
+		return ResponseEntity.internalServerError().body(errorResponse);
+	}
+
 }
