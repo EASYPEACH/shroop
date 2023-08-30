f1cf0bb63fa6d3342ccfea06958850c32afbad8f
박지윤 <jyun1937@naver.com>
2023-08-31 07:17:56 +0900

    Refactor: 사용하지 않는 메서드 삭제 Feat: 상품기준으로 삭제하는 메서드 추가

--- backend/src/main/java/com/easypeach/shroop/modules/likes/domain/LikeRepository.java
+++ backend/src/main/java/com/easypeach/shroop/modules/likes/domain/LikeRepository.java
@@ -14,15 +14,11 @@ public interface LikeRepository extends JpaRepository<Likes, Long> {
 
 	List<Likes> findAllByMember(Member member);
 
-	Optional<Likes> findByMember(Member member);
-
 	boolean existsLikesByMemberAndProduct(Member member, Product product);
 
 	Optional<Likes> findByMemberAndProduct(Member member, Product product);
 
-	default Likes getByMember(final Member member) {
-		return findByMember(member).orElseThrow(() -> new LikesProductNotFoundException("좋아요 기록이 없습니다"));
-	}
+	void deleteAllByProduct(Product product);
 
 	default Likes getByMemberAAndProduct(final Member member, final Product product) {
 		return findByMemberAndProduct(member, product).orElseThrow(
