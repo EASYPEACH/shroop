b5e894ff3fd0b76bfe96fb53fc5a80e2eeb7eea6
박지윤 <jyun1937@naver.com>
2023-08-31 12:28:56 +0900

    Design: 컨텐트 레이아웃 폭 조절

--- frontend/src/layouts/ContentLayout.vue
+++ frontend/src/layouts/ContentLayout.vue
@@ -6,11 +6,9 @@
 
 <style lang="scss" scoped>
 section {
-  width: 60%;
+  width: 70%;
   margin: 0 auto;
-  @media (max-width: 1200px) {
-    width: 70%;
-  }
+
   @media (max-width: 960px) {
     width: 90%;
   }
