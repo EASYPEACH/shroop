162f6c8987f0f5c7f71f7f8a0d6cd5e895604c92
jiny798 <jyoung7988@naver.com>
2023-08-31 19:49:57 +0900

    Feat: 마이페이지 정보 DTO 작성

--- backend/src/main/java/com/easypeach/shroop/modules/member/dto/reponse/LikeProductInfo.java
+++ backend/src/main/java/com/easypeach/shroop/modules/member/dto/reponse/LikeProductInfo.java
@@ -1,7 +1,5 @@
 package com.easypeach.shroop.modules.member.dto.reponse;
 
-import org.springframework.data.domain.Page;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +7,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class MyPageInfoResponse {
-	private String userImg;
-	private String nickname;
-	private Long point;
-	private Page<LikeProductInfo> page;
+public class LikeProductInfo {
+	private Long productId;
+	private String productImgUrl;
+	private String productName;
+	private Long productPrice;
 }
