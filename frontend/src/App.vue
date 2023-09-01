a3f517d38471449861d6d9d4cd03f2d317dcc334
박지윤 <jyun1937@naver.com>
2023-09-01 11:29:34 +0900

    Feat: interceptor 예외처리 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,8 @@
 import { useApiLoading } from "@/store/useLoading";
 import api from "./api";
 
+const loadingStore = useApiLoading();
+
 api.interceptors.request.use(
   (config) => {
     if (
@@ -15,23 +17,33 @@ api.interceptors.request.use(
         config.url.includes("phone")
       )
     ) {
-      const loadingStore = useApiLoading();
       loadingStore.setIsLoading(true);
     }
 
     return config;
   },
   (err) => {
+    loadingStore.setIsLoading(false);
+    if (err.response.status === 500) {
+      alert(
+        "서버에 문제가 생겼습니다. 새로고침을 하거나 관리자에게 문의해주세요.",
+      );
+    }
     return Promise.reject(err);
   },
 );
 api.interceptors.response.use(
   (config) => {
-    const loadingStore = useApiLoading();
     loadingStore.setIsLoading(false);
     return config;
   },
   (err) => {
+    loadingStore.setIsLoading(false);
+    if (err.response.status === 500) {
+      alert(
+        "서버에 문제가 생겼습니다. 새로고침을 하거나 관리자에게 문의해주세요.",
+      );
+    }
     return Promise.reject(err);
   },
 );
