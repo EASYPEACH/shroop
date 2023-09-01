339c49ccb3223d1530aab1e40ba4252798526a14
{dlwhdals5059} <dlwhdals5059@gmail.com>
2023-09-01 10:29:46 +0900

    Chore: BankService 생성

---
+++ backend/src/main/java/com/easypeach/shroop/modules/bank/service/BankService.java
@@ -0,0 +1,14 @@
+package com.easypeach.shroop.modules.bank.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class BankService {
+}
