3e8c40a7a3a8736c3c51f70fb24917bda275f64a
jiny798 <jyoung7988@naver.com>
2023-09-01 10:51:47 +0900

    Refactor: 프론트에서 화폐 단위로 볼수 있도록 dto 수정

--- backend/src/main/java/com/easypeach/shroop/modules/member/dto/reponse/LikeProductInfo.java
+++ backend/src/main/java/com/easypeach/shroop/modules/member/dto/reponse/LikeProductInfo.java
@@ -1,5 +1,8 @@
 package com.easypeach.shroop.modules.member.dto.reponse;
 
+import com.easypeach.shroop.modules.likes.domain.Likes;
+import com.easypeach.shroop.modules.product.domain.Product;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +14,14 @@ public class LikeProductInfo {
 	private Long productId;
 	private String productImgUrl;
 	private String productName;
-	private Long productPrice;
+	private String productPrice;
+
+	public LikeProductInfo(Likes likes){
+		Product product = likes.getProduct();
+		productId = product.getId();
+		productImgUrl = product.getProductImgList().get(0).getProductImgUrl();
+		productName = product.getTitle();
+		productPrice = String.format("%,d",product.getPrice());
+
+	}
 }
