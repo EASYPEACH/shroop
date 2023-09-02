800a867fcf4f90f64ee53965c9bbaed561e541d1
{dlwhdals5059} <dlwhdals5059@gmail.com>
2023-09-03 00:53:15 +0900

    Feat: 계좌 연동하기 API 구현

---
+++ backend/src/main/java/com/easypeach/shroop/modules/bank/dto/LinkBankRequest.java
@@ -0,0 +1,21 @@
+package com.easypeach.shroop.modules.bank.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LinkBankRequest {
+
+	@NotBlank
+	@Size(min = 2, max = 255, message = "이름은 최소 2자 이상 입력해주세요")
+	private String name;
+
+	@NotBlank
+	@Pattern(regexp = "^(\\d{1,})(-(\\d{1,})){1,}", message = "계좌번호 형식이 잘못되었습니다.")
+	private String account;
+}
--- backend/src/main/java/com/easypeach/shroop/modules/bank/service/BankService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/bank/service/BankService.java
@@ -5,8 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.easypeach.shroop.modules.bank.domain.Bank;
 import com.easypeach.shroop.modules.bank.domain.BankRepository;
+import com.easypeach.shroop.modules.bank.dto.LinkBankRequest;
 import com.easypeach.shroop.modules.bank.exception.MinusMoneyException;
 import com.easypeach.shroop.modules.member.domain.Member;
+import com.easypeach.shroop.modules.member.domain.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BankService {
 	private final BankRepository bankRepository;
+	private final MemberRepository memberRepository;
 
 	@Transactional
 	public void subtractMoney(final Long point, final Member member) {
@@ -33,5 +36,14 @@ public class BankService {
 	public void addPoint(final Long point, final Member member) {
 		Bank bank = bankRepository.getByAccount(member.getAccount());
 		bank.addMoney(point);
+	}
+
+	@Transactional
+	public void linkingAccount(final LinkBankRequest linkBankRequest, final Member member) {
+		// member에 계좌를 넣어주고, bank에 새로운 계좌 등록
+		Member foundMember = memberRepository.getById(member.getId());
+		foundMember.updateAccount(linkBankRequest.getAccount());
+		Bank bank = Bank.createBank(linkBankRequest.getName(), linkBankRequest.getAccount());
+		bankRepository.save(bank);
 	}
 }
