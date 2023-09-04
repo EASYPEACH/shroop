db31e85ac3f7a35a1463fe4037e52625e479623d
{dlwhdals5059} <dlwhdals5059@gmail.com>
2023-09-04 17:40:12 +0900

    Refactor: 이름 일치여부 로직 추가, 메소드 분리, 더미 데이터 생성

--- backend/src/main/java/com/easypeach/shroop/modules/bank/service/BankService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/bank/service/BankService.java
@@ -43,27 +43,42 @@ public class BankService {
 
 	@Transactional
 	public void linkingAccount(final LinkBankRequest linkBankRequest, final Member member) {
-		String EnteredAccount = linkBankRequest.getAccount();
-		String EnteredPassword = linkBankRequest.getPassword();
-		String encodedEnteredPassword = passwordEncoder.encode(EnteredPassword);
 
 		Member foundMember = memberRepository.getById(member.getId());
-		String encodedBankPassword = foundMember.getPassword();
-		String BankAccount = foundMember.getAccount();
 
-		boolean isMatchedPassword = passwordEncoder.matches(encodedEnteredPassword, encodedBankPassword);
-		boolean isMatchedAccount = EnteredAccount.matches(BankAccount);
-
-		if (isMatchedPassword && isMatchedAccount) {
+		boolean isAuthenticate = authenticateAccount(linkBankRequest, member);
+		if (isAuthenticate) {
 			foundMember.updateAccount(linkBankRequest.getAccount());
 		} else {
 			throw new AccountMismatchException("계좌번호가 일치하지 않습니다.");
 		}
 	}
 
+	@Transactional
 	public void creatingAccount(LinkBankRequest linkBankRequest) {
 		String encodedPassword = passwordEncoder.encode(linkBankRequest.getPassword());
 		Bank bank = Bank.createBank(linkBankRequest.getName(), linkBankRequest.getAccount(), encodedPassword);
 		bankRepository.save(bank);
+	}
+
+	public boolean authenticateAccount(LinkBankRequest linkBankRequest, Member member) {
+		String enteredName = linkBankRequest.getName();
+		String enteredAccount = linkBankRequest.getAccount();
+		String enteredPassword = linkBankRequest.getPassword();
+
+		String encodedEnteredPassword = passwordEncoder.encode(enteredPassword);
+		
+		Bank foundBank = bankRepository.getByAccount(enteredAccount);
+
+		String bankName = foundBank.getName();
+		String encodedBankPassword = foundBank.getPassword();
+
+		boolean isMatchedPassword = passwordEncoder.matches(encodedEnteredPassword, encodedBankPassword);
+		boolean isMatchedName = bankName.matches(enteredName);
+		if (isMatchedPassword && isMatchedName) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
