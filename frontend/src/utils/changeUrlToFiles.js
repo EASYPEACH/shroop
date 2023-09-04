e04ffedf647d7c8694d9cc5b5ff86c9be59e52f3
박지윤 <jyun1937@naver.com>
2023-09-04 16:14:56 +0900

    Fix: s3 cors 오류 해결

--- frontend/src/utils/changeUrlToFiles.js
+++ frontend/src/utils/changeUrlToFiles.js
@@ -1,7 +1,9 @@
 export const changeUrlToFiles = async (imglist) => {
   let transfer = new DataTransfer();
   for (let i = 0; i < imglist.length; i++) {
-    const response = await fetch(imglist[i]);
+    const response = await fetch(imglist[i], {
+      cache: "no-cache",
+    });
     const blob = await response.blob();
     const file = new File([blob], `image${i}.jpg`, { type: blob.type });
     transfer.items.add(file).getAsFile();
