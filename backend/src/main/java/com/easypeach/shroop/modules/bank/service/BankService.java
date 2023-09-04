bcde7fa898a0880e658911938b84636557b04914
{dlwhdals5059} <dlwhdals5059@gmail.com>
2023-09-04 17:21:10 +0900

    Refactor: 계좌번호 일치 여부 추가

--- backend/src/main/java/com/easypeach/shroop/modules/bank/service/BankService.java
+++ backend/src/main/java/com/easypeach/shroop/modules/bank/service/BankService.java
@@ -43,14 +43,18 @@ public class BankService {
 
 	@Transactional
 	public void linkingAccount(final LinkBankRequest linkBankRequest, final Member member) {
+		String EnteredAccount = linkBankRequest.getAccount();
 		String EnteredPassword = linkBankRequest.getPassword();
 		String encodedEnteredPassword = passwordEncoder.encode(EnteredPassword);
 
 		Member foundMember = memberRepository.getById(member.getId());
 		String encodedBankPassword = foundMember.getPassword();
+		String BankAccount = foundMember.getAccount();
 
 		boolean isMatchedPassword = passwordEncoder.matches(encodedEnteredPassword, encodedBankPassword);
-		if (isMatchedPassword) {
+		boolean isMatchedAccount = EnteredAccount.matches(BankAccount);
+
+		if (isMatchedPassword && isMatchedAccount) {
 			foundMember.updateAccount(linkBankRequest.getAccount());
 		} else {
 			throw new AccountMismatchException("계좌번호가 일치하지 않습니다.");
