4102ee53c692f73675f4a3823eba571bf2ca993f
박진영 <122027452+jiny798@users.noreply.github.com>
2023-09-06 19:37:57 +0900

    Feat: 회원 탈퇴 처리 로직 구현 (#343)

---
+++ backend/src/main/java/com/easypeach/shroop/modules/member/dto/request/MemberInfo.java
@@ -0,0 +1,12 @@
+package com.easypeach.shroop.modules.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberInfo {
+	private String password;
+}
