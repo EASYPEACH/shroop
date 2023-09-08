465aea5e59e9c614da616129488f306410db1254
jiny798 <jyoung7988@naver.com>
2023-09-08 15:55:42 +0900

    Refactor: 차단,탈퇴 계정 구분하도록 수정 및 응답 메세지 추가

--- backend/src/main/java/com/easypeach/shroop/modules/member/domain/Role.java
+++ backend/src/main/java/com/easypeach/shroop/modules/member/domain/Role.java
@@ -1,5 +1,18 @@
 package com.easypeach.shroop.modules.member.domain;
 
 public enum Role {
-	ROLE_DELETE, ROLE_NOT_AUTH_USER, ROLE_USER, ROLE_ADMIN
+	ROLE_DELETE("탈퇴한 계정입니다"),
+	ROLE_NOT_AUTH_USER("차단된 계정입니다"),
+	ROLE_USER("로그인에 성공하셨습니다"),
+	ROLE_ADMIN("관리자 계정으로 로그인하였습니다");
+
+	private String responseMessage;
+
+	Role(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+
+	public String getResponseMessage() {
+		return this.responseMessage;
+	}
 }
