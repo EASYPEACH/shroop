c52af9080165614c068b864b6c05c87b2f392c07
jiny798 <jyoung7988@naver.com>
2023-09-08 15:56:37 +0900

    Refactor: 차단,탈퇴된 계정은 404 로그인 실패시 400 응답

---
+++ backend/src/main/java/com/easypeach/shroop/modules/auth/dto/response/LoginFailResponse.java
@@ -0,0 +1,12 @@
+package com.easypeach.shroop.modules.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginFailResponse {
+	private String message;
+}
