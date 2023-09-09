570b7be2141ef9ff29d6f0af0fbc572580d5c26e
박진영 <122027452+jiny798@users.noreply.github.com>
2023-09-09 13:37:29 +0900

    Fix: 카테고리 엔티티를 DTO로 변경한다 (#411)
    * Fix: 카테고리 엔티티를 DTO로 변경
    
    * Test: 카테고리 DTO 변경에 따라 테스트 코드 수정

--- backend/src/main/java/com/easypeach/shroop/modules/member/dto/reponse/LikeProductInfo.java
+++ backend/src/main/java/com/easypeach/shroop/modules/member/dto/reponse/LikeProductInfo.java
@@ -1,6 +1,5 @@
 package com.easypeach.shroop.modules.member.dto.reponse;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.easypeach.shroop.modules.likes.domain.Likes;
@@ -19,17 +18,30 @@ public class LikeProductInfo {
 	private String productImgUrl;
 	private String title;
 	private String price;
-	private Category category;
+	private CategoryInfo categoryInfo;
 	private LocalDateTime createDate;
 
-	public LikeProductInfo(Likes likes){
+	public LikeProductInfo(Likes likes) {
 		Product product = likes.getProduct();
 		id = product.getId();
 		productImgUrl = product.getProductImgList().get(0).getProductImgUrl();
 		title = product.getTitle();
-		price = String.format("%,d",product.getPrice());
-		category = product.getCategory();
+		price = String.format("%,d", product.getPrice());
+		categoryInfo = new CategoryInfo(product.getCategory());
 		createDate = product.getCreateDate();
 
+	}
+
+	@Getter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class CategoryInfo {
+		private Long id;
+		private String name;
+
+		public CategoryInfo(Category category) {
+			id = category.getId();
+			name = category.getName();
+		}
 	}
 }
