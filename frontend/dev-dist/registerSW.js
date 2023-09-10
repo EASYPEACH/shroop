556403ec6d667866dabcd0e2a6dec258c8bb62dc
박지윤 <jyun1937@naver.com>
2023-09-10 16:53:27 +0900

    Fix:manifest.json 경로 수정

--- frontend/dev-dist/registerSW.js
+++ frontend/dev-dist/registerSW.js
@@ -1,1 +1,5 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+if ("serviceWorker" in navigator)
+  navigator.serviceWorker.register("/dev-sw.js?dev-sw", {
+    scope: "/",
+    type: "classic",
+  });
