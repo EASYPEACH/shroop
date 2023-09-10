4e50292662a32f9a9dd28b8edf9a6791a1e41225
박지윤 <jyun1937@naver.com>
2023-09-10 21:19:25 +0900

    Refactor: 구매내역 조회 페이징 API로 수정

--- frontend/src/views/mypage/MyPagePurchaseListView.vue
+++ frontend/src/views/mypage/MyPagePurchaseListView.vue
@@ -5,25 +5,22 @@
         v-if="purchaseList.length === 0"
         title="구매내역이 없습니다"
       />
-      <li
-        v-for="product in purchaseList.slice(startIndex, endIndex)"
-        :key="product.id"
-      >
+      <li v-for="product in purchaseList" :key="product.id">
         <mypage-product-banner :product="product" isStatus />
       </li>
     </ul>
     <v-pagination
-      v-model="currentPage"
-      :length="pageCount"
+      v-model="purchasePage"
+      :length="purchasePageCount"
       :total-visible="isTablet ? 3 : 5"
-      @click="handleChangePurchasePage"
+      @click="handleGetPurchaseHistory"
     >
     </v-pagination>
   </content-layout>
 </template>
 
 <script setup>
-import { ref, onBeforeMount, computed, watch } from "vue";
+import { ref, onBeforeMount } from "vue";
 import { useDisplay } from "vuetify";
 import { getApi } from "@/api/modules";
 import { InfoAlert } from "@/components/Alert";
@@ -33,53 +30,29 @@ import ContentLayout from "@/layouts/ContentLayout.vue";
 const display = useDisplay();
 const isTablet = ref(display.smAndDown);
 
-const perPage = ref(5); // 페이지당 상품 수
-const currentPage = ref(1); // 현재 페이지
-const startIndex = ref(0); // 상품 시작 인덱스
-const endIndex = ref(perPage.value); // 상품 마지막 인덱스
-
-// 구매내역
 const purchaseList = ref([]);
+const purchasePageCount = ref(0);
+const purchasePage = ref(1);
 
 onBeforeMount(async () => {
   await handleGetPurchaseHistory();
 });
 
-// 상품 수 계산
-const productCount = computed(() => {
-  return purchaseList.value.length;
-});
-
-// 페이지 수 계산
-const pageCount = computed(() => {
-  return Math.ceil(productCount.value / perPage.value);
-});
-
-watch(purchaseList, () => {
-  currentPage.value = 1;
-  handleChangePurchasePage();
-});
-
 // 구매내역
 const handleGetPurchaseHistory = async () => {
   try {
-    const purchaseData = await getApi({ url: "/api/buying/history" });
+    const purchaseData = await getApi({
+      url: `/api/history/buying?page=${
+        purchasePage.value - 1
+      }&size=5&sort=createDate,desc`,
+    });
     if (purchaseData !== null) {
-      purchaseList.value = purchaseData;
+      purchaseList.value = purchaseData.historyResponseList;
+      purchasePageCount.value = purchaseData.pageCount;
     }
   } catch (error) {
     console.error(error);
   }
-};
-
-// 페이징 변경 핸들러
-const handleChangePurchasePage = () => {
-  startIndex.value = (currentPage.value - 1) * perPage.value;
-  endIndex.value = Math.min(
-    startIndex.value + perPage.value,
-    productCount.value,
-  );
-  window.scrollTo({ top: 0 });
 };
 </script>
 
