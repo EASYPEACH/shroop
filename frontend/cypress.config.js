e9ae17f46ebc2e1d7affe406bf43b8c9b69db13c
kongtaesic <xotlr333@naver.com>
2023-09-13 17:45:21 +0900

    Refactor: cypress 자동화 옵션

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -12,5 +12,6 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:3000",
+    experimentalStudio: true,
   },
 });
