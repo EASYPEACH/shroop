b29b5afadf564bd8ad8dfa8507dc72ec4499d766
TAESIC KONG <81614820+xotlr333@users.noreply.github.com>
2023-09-13 12:21:13 +0900

    Refactor: 낙관전락 이후 재시도하도록 수정 (#500)

---
+++ backend/src/main/java/com/easypeach/shroop/infra/aop/likes/Retry.java
@@ -0,0 +1,12 @@
+package com.easypeach.shroop.infra.aop.likes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Retry {
+	int value() default 5;
+}
---
+++ backend/src/main/java/com/easypeach/shroop/infra/aop/likes/RetryAspect.java
@@ -0,0 +1,31 @@
+package com.easypeach.shroop.infra.aop.likes;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
+@Aspect
+public class RetryAspect {
+
+	@Around("@annotation(retry)")
+	public Object doRetry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+		int maxRetry = retry.value();
+
+		Exception exceptionHolder = null;
+		for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+			try {
+				return joinPoint.proceed();
+			} catch (Exception e) {
+				log.error("[retry] try count ={}/{}", retryCount, maxRetry);
+				exceptionHolder = e;
+			}
+		}
+		throw exceptionHolder;
+	}
+}
