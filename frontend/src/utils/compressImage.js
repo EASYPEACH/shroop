6c96244ac0cfb1319678338eb28b18ce86a124f3
JIYUN <72537762+Jiyun-Parkk@users.noreply.github.com>
2023-09-15 15:09:25 +0900

    Fix: 아이폰 이미지 확장자 변경 라이브러리 추가 (#571)
    * Refactor: 이미지 최적화를 진행했다
    
    * Chore: cypress dev dependency로 변경
    
    * Fix: 조건문을 삭제했다
    
    * Fix: textarea 설정 변경
    
    * Test:이미지 압축 결과 테스트 콘솔
    
    * Fix: 이미지 압축 조건 수정
    
    * Fix: 이미지 압축 조건문 수정
    
    * Fix: 이미지 변환 조건 수정
    
    * Fix: 이미지 변환 조건 수정
    
    * Fix: 이미지 변환 조건 수정
    
    * Fix: 아이폰 확장자 jpeg로 변경하는 코드 추가
    
    ---------
    
    Co-authored-by: 박진영 <122027452+jiny798@users.noreply.github.com>

--- frontend/src/utils/compressImage.js
+++ frontend/src/utils/compressImage.js
@@ -1,5 +1,6 @@
 import { useApiLoading } from "@/store/modules";
 import Compressor from "compressorjs";
+import heic2any from "heic2any";
 
 export const compressImage = (files, inputRef) => {
   console.log(files);
@@ -8,31 +9,51 @@ export const compressImage = (files, inputRef) => {
     const loadingStore = useApiLoading();
     loadingStore.setIsLoading(true);
     Array.from(files).forEach((file) => {
-      new Compressor(file, {
-        minWidth: 450,
-        minHeight: 450,
-        quality: 0.1,
-        convertTypes: ["image/png", "image/webp"],
-        mimeType: "image/webp",
-        convertSize: 5000000,
-        success: function (result) {
-          const newFile = new File([result], `image${new Date().getTime()}`, {
-            type: result.type,
-          });
-          transfer.items.add(newFile).getAsFile();
-          if (inputRef.value.input !== undefined) {
-            inputRef.value.input.files = transfer.files;
-          } else {
-            inputRef.value.files = transfer.files;
-          }
-          console.log(newFile);
-
-          loadingStore.setIsLoading(false);
-        },
-        error: function (err) {
-          console.log(err);
-        },
-      });
+      if (file.name.split(".")[1] === "heic") {
+        // blob에다가 변환 시키고 싶은 file값을 value로 놓는다.
+        // toType에다가는 heic를 변환시키고싶은 이미지 타입을 넣는다.
+        heic2any({ blob: file, toType: "image/jpeg" }).then(
+          function (resultBlob) {
+            //file에 새로운 파일 데이터를 씌웁니다.
+            file = new File([resultBlob], file.name.split(".")[0] + ".jpg", {
+              type: "image/jpeg",
+              lastModified: new Date().getTime(),
+            });
+            returnCompressor(file, transfer, inputRef, loadingStore);
+          },
+        );
+      } else {
+        returnCompressor(file, transfer, inputRef, loadingStore);
+      }
     });
   }
+};
+
+const returnCompressor = (file, transfer, inputRef, loadingStore) => {
+  return new Compressor(file, {
+    minWidth: 450,
+    minHeight: 450,
+    quality: 0.8,
+    convertTypes: ["image/png", "image/webp"],
+    mimeType: "image/webp",
+    convertSize: 500,
+    success: function (result) {
+      const newFile = new File([result], `image${new Date().getTime()}`, {
+        type: result.type,
+      });
+
+      transfer.items.add(newFile).getAsFile();
+      if (inputRef.value.input !== undefined) {
+        inputRef.value.input.files = transfer.files;
+      } else {
+        inputRef.value.files = transfer.files;
+      }
+      console.log(newFile);
+
+      loadingStore.setIsLoading(false);
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  });
 };
